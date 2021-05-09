cbacf9c28afcc390bfe1f297751976b9dd58d69f
FaranMustafa <fmustafa044@gmail.com>
2021-05-09 12:59:03 +0500

    update: main image change

--- src/assets/images/MainImages/index.js
+++ src/assets/images/MainImages/index.js
@@ -1,5 +1,5 @@
-import MainImage1 from './main-image1.svg';
-import MainImage2 from './main-image2.svg';
-import MainImage3 from './main-image3.svg';
+import MainImage1 from './1.png';
+import MainImage2 from './2.png';
+import MainImage3 from './3.png';
 
 export { MainImage1, MainImage2, MainImage3 };
